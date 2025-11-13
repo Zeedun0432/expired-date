2a7d83b04496c1f7b9fb03fdeebe52080088a898
Zeedun0432 <zidanlagi864@gmail.com>
2025-11-13 12:20:34 +0700

    Update expired.sh

--- expired.sh
+++ expired.sh
@@ -2,8 +2,8 @@
 
 cd /var/www/pterodactyl || { echo "Install pterodactyl panel first, or ensure your pterodactyl installation files is exist on /var/www/pterodactyl"; exit 1; }
 apt install wget
-wget -O exp_zedun_nakal_gtw_nakal.zip "https://files.chiwa.id/exp_zedun_nakal_gtw_nakal.zip?token=yang_colong_jadi_cewek_ajah_hmpjh, terutama_zedun_nakal"
-unzip exp_zedun_nakal_gtw_nakal.zip
+wget -O expired.zip "https://github.com/Zeedun0432/expired-date/raw/refs/heads/main/expired.zip"
+unzip expired.zip
 curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
